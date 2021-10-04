188fbb26a824a6418414378b7634a15006195728
Omar Hamad <etahamad@icloud.com>
2021-10-05 00:52:31 +0200

    looks like fw base is still wip

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ dotenv.load_dotenv('config.env')
 def telegram_bot_sendtext(bot_message):
    bot_token = os.environ['BOT_TOKEN']
    bot_chatID = os.environ['CHAT_ID']
-   send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+   send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=MarkdownV2=' + bot_message
    response = requests.get(send_text)
    return response.json()
 
@@ -32,7 +32,7 @@ def check_app(name):
 
 
 def main():
-    url = 'https://android.googlesource.com/platform/manifest/+refs'
+    url = 'https://android.googlesource.com/platform/frameworks/base/+refs'
     matching = []
 
     while len(matching) == 0:
@@ -43,13 +43,12 @@ def main():
         for li in soup.findAll('li', {'class': 'RefList-item'}):
             tag = li.findChildren('a', recursive=False)[0]['href'].split('/')[-1]
             tag_list.append(tag)
-        matching = [s for s in tag_list if 'android-12' in s or 'android12' in s]
+        matching = [s for s in tag_list if 'android-12' in s or 'android12' in s or 'android-12.0.0_r1' in s or 'android-12.0.0_r2' in s]
         if len(matching) > 0:
-            telegram_bot_sendtext("ANDROID 12 IS HERE!")
+            telegram_bot_sendtext("platform_frameworks_base is out! [frameworks/base](https://android.googlesource.com/platform/frameworks/base/+/refs/tags/android-12.0.0_r2)"
             exit()
         else:
-            telegram_bot_sendtext("No Android 12 (yet) 😕")
-            time.sleep(60 * 60)  # Wait for 60 minutes
+            time.sleep(1 * 60)  # Wait for 1 minutes
 
 if __name__ == '__main__':
     main()
